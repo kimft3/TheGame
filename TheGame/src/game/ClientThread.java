11088fbc70244afea6c86adb1ba98e02cbfa554b
Tais <55287175+RocketBadger@users.noreply.github.com>
2020-10-03 17:43:04 +0200

    Fjernet syncvhronized, wait() og notify() fra ClientThread

--- TheGame/src/game/ClientThread.java
+++ TheGame/src/game/ClientThread.java
@@ -27,34 +27,11 @@ public class ClientThread extends Thread {
 				playerInfo = inFromServer.readLine().split("#");
 				ClientGame.flytterundt(Integer.parseInt(playerInfo[2]), Integer.parseInt(playerInfo[3]),
 						Integer.parseInt(playerInfo[4]), Integer.parseInt(playerInfo[5]), playerInfo[6]);
+				ClientGame.updateScore(playerInfo[0], playerInfo[1]);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
-//		 		System.out.println(inFromServer.readLine());
-// 				playerData=p.getName()+"#"+p.getPoint()+"#"+p.getXposOld()+"#"+p.getYposOld()+"#"+p.getXpos()+"#"+p.getYpos()+"#"+p.getDirection();
-//				wait();
-
-			try {
-				crit(playerInfo);
-			} catch (Exception e) {
-				// TODO: handle exception
-				System.out.println("ClientThread RUN() Error: " + e);
-				e.printStackTrace();
-			}
-			notify();
 		}
-	}
-
-	public synchronized void crit(String[] playerInfo) {
-		try {
-			wait(10);
-		} catch (InterruptedException e) {
-			System.out.println("ClientThread CRIT() Error: " + e);
-			e.printStackTrace();
-		}
-		ClientGame.updateScore(playerInfo[0], playerInfo[1]);
-		notify();
 	}
 
 }
